0f0de9bfcd831bed148a13e4c038b2757091be71
Francisco <franciscovicenzi@gmail.com>
2019-04-22 13:31:49 -0300

    Adicionado grmáticas

--- dfa.py
+++ dfa.py
@@ -1,4 +1,3 @@
-import copy
 class DFA:
     def __init__(self, states, alphabet, init_state, final_states, transitions):
         self.states = states
@@ -11,7 +10,7 @@ class DFA:
         
     def make_transition(self, symbol):
         print('Current state before: ', self.current_state, '| Symbol: ', symbol)
-        aux = transitions[self.current_state][symbol]
+        aux = self.transitions[self.current_state][symbol]
         self.current_state = self.dead_state if aux == '' else aux
         print('Current state after: ', self.current_state)
     
@@ -27,3 +26,13 @@ class DFA:
                 break
         print('End state: ', self.current_state)
         return True if self.current_state in self.final_states else False
+
+    def to_grammar(self):
+        grQ = {k: {} for k in self.states}
+        for k, x in self.transitions.items():
+            aux = []
+            for q, t in x.items():
+                aux.append(q+t)
+                if t in self.final_states: aux.append(q)
+            grQ[k] = aux
+        return grQ
