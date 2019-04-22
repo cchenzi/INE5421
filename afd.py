9dae34529778298d7063b08b003b84d89d50dc95
Francisco <franciscovicenzi@gmail.com>
2019-04-22 13:29:18 -0300

    Adicionado suporte à gramáticas.

--- afd.py
+++ afd.py
@@ -1,4 +1,3 @@
-import copy
 class DFA:
     def __init__(self, states, alphabet, init_state, final_states, transitions):
         self.states = states
@@ -8,13 +7,13 @@ class DFA:
         self.init_state = init_state
         self.current_state = init_state
         self.dead_state = 'qdead'
-        
+
     def make_transition(self, symbol):
         print('Current state before: ', self.current_state, '| Symbol: ', symbol)
-        aux = transitions[self.current_state][symbol]
+        aux = self.transitions[self.current_state][symbol]
         self.current_state = self.dead_state if aux == '' else aux
         print('Current state after: ', self.current_state)
-    
+
     def reset_init_state(self):
         self.current_state = self.init_state
 
@@ -27,3 +26,13 @@ class DFA:
                 break
         print('End state: ', self.current_state)
         return True if self.current_state in self.final_states else False
+
+    def to_grammar(self):
+        grQ = {k: {} for k in self.states}
+        for k, x in self.transitions.items():
+            aux = []
+            for q, t in x.items():
+                aux.append(q+t)
+                if t in self.final_states: aux.append(q)
+            grQ[k] = aux
+        return grQ
