b8b4a5887a7c5a24315608fdc4c32b2a01319294
Arthur Mesquita Pickcius <arthur.pickcius@gmail.com>
2019-04-28 21:06:05 -0300

    set new states

--- nfa.py
+++ nfa.py
@@ -60,68 +60,6 @@ class NFA:
             newdd[k] = list(set([item for sublist in newdd[k] for item in sublist]))
         return newdd
 
-class NFA:
-    def __init__(self, states, alphabet, init_state, final_states,
-            transitions, has_epsilon):
-        self.states = states
-        self.alphabet = alphabet
-        self.final_states = final_states
-        self.transitions = transitions
-        self.init_state = init_state
-        self.current_state = init_state
-        if has_epsilon:
-            self.epsilon_closure = dict.fromkeys(transitions)
-        self.dead_state = 'qdead'
-
-    def make_transition(self, symbol):
-        print(f'Current state before: {self.current_state} | Symbol: {symbol}')
-        aux = self.transitions[self.current_state][symbol]
-        self.current_state = self.dead_state if aux == '' else aux
-        print(f'Current state after: {self.current_state}')
-
-    def reset_init_state(self):
-        self.current_state = self.init_state
-
-    def is_input_valid(self, input):
-        self.reset_init_state()
-        print(f'\nStarting to check if {input} is valid...')
-        for x in input:
-            self.make_transition(x)
-            if self.current_state == self.dead_state:
-                break
-        print(f'End state: {self.current_state}')
-        return True if self.current_state in self.final_states else False
-
-    def compute_epsilon_closure(self):
-        for k, tr in self.transitions.items():
-            aux = []
-            aux.append(k)
-            if tr['&'] not in aux:
-                aux.append(tr['&'])
-            for y in aux:
-                if self.transitions[y]['&'] not in aux:
-                    aux.append(self.transitions[y]['&'])
-            print(aux)
-            self.epsilon_closure[k] = list(set(aux))
-
-    def get_epsilon_transitions(self, state):
-        newdd = {key: [] for key in self.alphabet}
-        print(self.states)
-        for x in self.states:
-            print('\n\n')
-            print('in: ', x)
-            for letter in self.alphabet:
-                aux = []
-                print('letter: ', letter)
-                print('transitions for x: ', self.transitions[x])
-                for t in self.transitions[x][letter]:
-                    if t != '':
-                        aux.append(self.epsilon_closure[t])
-                flat_list = [item for sublist in aux for item in sublist]
-        for k, x in newdd.items():
-            newdd[k] = list(set([item for sublist in newdd[k] for item in sublist]))
-        return newdd
-
     def minimize(self):
         self.discard_dead()
         self.discard_unreachable()
@@ -171,9 +109,12 @@ class NFA:
                 for t_state, new_state in self.transitions[state].items():
                     # t_state = '0'
                     # new_state = ['q0','q1']
-                    print(f'values: {new_states.values()}')
+                    for val in new_states.values():
+                        print(f'val: {val}')
                     print(f'new_state: {new_state}')
-                    if not any(set(new_state) == set(val) for val in new_states.values()):
+                    if (bool(new_state)
+                        and not any(set(new_state) == set(nsv) for nsv in new_states.values())
+                        and not any(set(new_state) == set(sq) for sq in state_queue)):
                         state_queue.append(new_state)
             index += 1
         print(new_states)
